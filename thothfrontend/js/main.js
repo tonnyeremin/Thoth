841746a0691ec448aab0b50081ca8a6b673fb41d
Tonny <an.eremin@gmail.com>
2020-04-30 19:27:54 +0200

    put

--- thothfrontend/js/main.js
+++ thothfrontend/js/main.js
@@ -1,8 +1,8 @@
 
 const api = "https://thothwebapp.azurewebsites.net/api/quoteitem";
 $(document).ready(function(){
-$('#showrandombtn').click(showrandom)
-$('#submitform').submit(submit)
+    $('#showrandombtn').click(showrandom)
+    $('#submitform').submit(submit)
 });
 
 function showrandom(data){
@@ -20,10 +20,24 @@ function showrandom(data){
      $.ajax({
         url: api,
         type: 'post',
-        dataType: 'application/json',
-        data: $("#submitform").serialize(),
+        crossdomain: true,
+        contentType: 'application/json',
+        dataType: 'json',
+        processData: false,
+        data: JSON.stringify(objectifyForm($('#submitform').serializeArray())),
         success: function(data) {
+          console.log(data);
           
         }
 });
- }     
+
+ }  
+ 
+ function objectifyForm(formArray) {//serialize data function
+
+    var returnArray = {};
+    for (var i = 0; i < formArray.length; i++){
+      returnArray[formArray[i]['name']] = formArray[i]['value'];
+    }
+    return returnArray;
+  };
